429736a6dbc67c4210e47dbd65d34fd1eebd9f33
Lucas Kruschewsky <lucas_kruschewsky@epam.com>
2022-02-17 16:34:53 +0000

    testing husky

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById("root"));
+ReactDOM.render(<h1>Hello World</h1>,
+    document.getElementById("root"));
