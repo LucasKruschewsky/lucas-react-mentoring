dea28b89c91a5ede01b632f5a73eda424cda9bea
Lucas Kruschewsky <lucas_kruschewsky@epam.com>
2022-02-17 16:41:42 +0000

    testing lint-staged integration

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-ReactDOM.render(<h1>Hello World</h1>,
-    document.getElementById("root"));
+ReactDOM.render(
+  <h1>Hello World</h1>,
+
+  document.getElementById("root")
+);
