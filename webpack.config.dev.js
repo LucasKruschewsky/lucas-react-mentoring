82894f94634de1fbc700ffbba0f68df0c656e688
Lucas Kruschewsky <lucas_kruschewsky@epam.com>
2022-02-18 14:36:32 +0000

    added speed-measure to webpack removed duplicated babel rule

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,11 +1,14 @@
-const path = require("path");
-const { merge } = require("webpack-merge");
-const common = require("./webpack.common");
+const path = require('path');
+const { merge } = require('webpack-merge');
+const SpeedMeasurePlugin = require('speed-measure-webpack-plugin');
+const common = require('./webpack.common');
 
-module.exports = merge(common, {
-  mode: "development",
+const smp = new SpeedMeasurePlugin();
+
+const mergedConfig = merge(common, {
+  mode: 'development',
   output: {
-    path: path.resolve(__dirname, "dev"),
+    path: path.resolve(__dirname, 'dev'),
   },
   devServer: {
     compress: true,
@@ -17,14 +20,16 @@ module.exports = merge(common, {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        enforce: "pre",
-        use: ["source-map-loader"],
+        enforce: 'pre',
+        use: ['source-map-loader'],
       },
       {
         test: /\.css$/i,
         exclude: /node_modules/,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
 });
+
+module.exports = smp.wrap(mergedConfig);
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,44 +1,34 @@
-const path = require("path");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const { merge } = require("webpack-merge");
-const common = require("./webpack.common");
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common');
 
 module.exports = merge(common, {
-  mode: "production",
+  mode: 'production',
   output: {
-    path: path.resolve(__dirname, "build"),
+    path: path.resolve(__dirname, 'build'),
   },
   module: {
     rules: [
       {
-        test: /\.?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
-          },
-        },
-      },
-      {
         test: /\.css$/i,
         exclude: /node_modules/,
         use: [
-          "style-loader",
+          'style-loader',
           {
             loader: MiniCssExtractPlugin.loader,
             options: {
               esModule: false,
             },
           },
-          "css-loader",
+          'css-loader',
         ],
       },
     ],
   },
   optimization: {
-    minimizer: ["...", new CssMinimizerPlugin()],
+    minimizer: new CssMinimizerPlugin(),
   },
   plugins: [new MiniCssExtractPlugin()],
 });
