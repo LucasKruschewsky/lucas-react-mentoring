204083c58b9e40ef673d2436880e4b2193e05f0d
Lucas Kruschewsky <lucas_kruschewsky@epam.com>
2022-03-16 14:04:12 +0000

    changed awesome-typescript-loader to ts-loader and updated ts to latest version, started migrating redux to redux-toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import Homepage from '@/pages/Homepage';
 import AppModalHandler from 'Components/ModalHandler';
 import { Provider } from 'react-redux';
 import store from '@/store';
+// import { ApiProvider } from '@reduxjs/toolkit/dist/query/react';
+// import { moviesApi } from './services/apiProvider';
 
 const App: React.FunctionComponent = () => (
   <Provider store={store}>
---
+++ src/services/apiProvider.ts
@@ -0,0 +1,14 @@
+import { IMoviesListData } from '@/data/MockedDataTypes';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const moviesApi = createApi({
+  reducerPath: 'moviesApi',
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.API_URL }),
+  tagTypes: ['Movies'],
+  endpoints: (builder) => ({
+    getAll: builder.query<IMoviesListData[], void>({
+      query: () => '/movies',
+      providesTags: [{ type: 'Movies', id: 'LIST' }],
+    }),
+  }),
+});
