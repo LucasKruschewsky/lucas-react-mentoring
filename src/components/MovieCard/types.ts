899d596f44141ad4beed695cd06aab022735be84
Lucas Kruschewsky <lucas_kruschewsky@epam.com>
2022-04-11 15:39:12 +0100

    changed some other strings to consts, added a component to check img url and display a fallback if 404 is returned

--- src/components/MovieCard/types.ts
+++ src/components/MovieCard/types.ts
@@ -1,5 +1,12 @@
 import { TMovieObject } from '@/store/modules/movieList/types';
 
+export interface ICardImage {
+  imgUrl: string;
+  imgAlt: string;
+  showHoverEffect: () => void;
+  hideHoverEffect: () => void;
+}
+
 export interface IMovieCardProps {
   movie: TMovieObject;
 }
--- src/services/axiosConfig.ts
+++ src/services/axiosConfig.ts
@@ -4,3 +4,7 @@ export const api = axios.create({
   baseURL: process.env.API_URL,
   validateStatus: (status) => status >= 200 && status < 500,
 });
+
+export const axiosImageCheck = axios.create({
+  validateStatus: (status) => status >= 200 && status < 500,
+});
